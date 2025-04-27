d40d41fb0125fee3161dc75230342a4487bec323
MICHAEL OUMA <oumamichael108@gmail.com>
2025-04-27 12:24:14 +0300

    Update PortfolioContainer.js

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,30 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({ portfolio, onSell }) {
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      <div className="portfolio">
+        {portfolio.length > 0 ? (
+          portfolio.map(stock => (
+            <div 
+              key={stock.id} 
+              onClick={() => onSell(stock.id)} 
+              style={{ cursor: 'pointer' }}
+            >
+              <Stock 
+                id={stock.id}
+                name={stock.name}
+                price={stock.price}
+                ticker={stock.ticker}
+              />
+            </div>
+          ))
+        ) : (
+          <p>Your portfolio is empty. Click on stocks to buy them!</p>
+        )}
+      </div>
     </div>
   );
 }
