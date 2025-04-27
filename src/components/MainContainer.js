1d551c43d930de926e9c742b3459eccd13cac6f0
MICHAEL OUMA <oumamichael108@gmail.com>
2025-04-27 12:24:51 +0300

    Update MainContainer.js

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,16 +3,59 @@ import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
-function MainContainer() {
+function MainContainer({
+  stocks,
+  portfolio,
+  sortBy,
+  filterBy,
+  onBuy,
+  onSell,
+  onSortChange,
+  onFilterChange,
+  isLoading,
+  error
+}) {
+  // Display loading state
+  if (isLoading) {
+    return (
+      <div className="container mt-5">
+        <h2>Loading stocks data...</h2>
+      </div>
+    );
+  }
+
+  // Display error state
+  if (error) {
+    return (
+      <div className="container mt-5">
+        <h2>Error loading stocks:</h2>
+        <p className="text-danger">{error}</p>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar 
+        sortBy={sortBy}
+        filterBy={filterBy}
+        onSortChange={onSortChange}
+        onFilterChange={onFilterChange}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer 
+            stocks={stocks}
+            sortBy={sortBy}
+            filterBy={filterBy}
+            onBuy={onBuy}
+          />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer 
+            portfolio={portfolio}
+            onSell={onSell}
+          />
         </div>
       </div>
     </div>
